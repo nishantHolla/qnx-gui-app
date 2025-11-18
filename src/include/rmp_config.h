4ed1772a5f6c8db7109a95e4c66c4f15838abe39
Nishant Holla <nishantholla19@gmail.com>
2025-11-18 23:53:20 +0530

    Update app config

--- src/include/rmp_config.h
+++ src/include/rmp_config.h
@@ -1,8 +1,5 @@
 #ifndef RMP_CONFIG_H_
-#define RMP_CONFIG_H_
 
-/// if 1 use keyboard else use keypad
-#define RMP_CONFIG_USE_KEYBOARD 0
-#define RMP_CONFIG_PAD_SPEED_BOOST 0
+#define RMP_USE_KEYBOARD 0
 
-#endif // RMP_CONFIG_H_
+#endif // !RMP_CONFIG_H_
